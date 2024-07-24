9b3b852ffac68ee20f9288d96336346a008c240f
Luca <luca_barreca@hotmail.it>
2024-07-24 16:45:36 +0200

    primoCommit

---
+++ data/sam/src/main/java/com/generation/italy/model/Role.java
@@ -0,0 +1,37 @@
+package com.generation.italy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String Role;
+	private Boolean isAdmin;
+
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return Role;
+	}
+	public void setRole(String role) {
+		Role = role;
+	}
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	
+}
---
+++ data/sam/src/main/java/com/generation/italy/model/User.java
@@ -0,0 +1,93 @@
+package com.generation.italy.model;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name="email", unique=true)
+    @Email(message="email non corretta")
+    @NotBlank(message="email obbligatoria")
+	private String email;
+	
+	@NotBlank(message="nome obbligatorio")
+	private String nomeCompleto;
+	
+	@NotBlank
+	@Length(min=8, max=60)
+	private String password;
+	
+	@ManyToOne
+	@JoinColumn(name = "role_id")
+	private Role role;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	
+	
+	
+}
+
+
+
+
+/*	
+private String taskname;
+private String status;
+private LocalDate data_inizio;
+private LocalDate data_fine;
+
+ @ManyToOne
+ @JoinColumn(mappedby = "")
+*/
